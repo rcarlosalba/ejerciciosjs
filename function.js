a02e7ad01d2709a35c36179d8f2a950be9b48ecd
Carlos Alba <hola@carlosalba.net>
2020-05-21 13:20:38 +0300

    día 1

--- function.js
+++ function.js
@@ -1,56 +1,42 @@
-var saludar = () => {
-  var nombre = document.getElementById('saludo');
-  var saludoContainer = document.getElementById('saludoContainer');
-  saludoContainer.innerHTML = "Hola " + nombre.value + " gracias por visitarnos"
+function saludo(){
+  let nombre = document.getElementById('nombre')
+  let saludo = document.getElementById('saludo')
+  saludo.innerHTML = `Hola ${nombre.value}`
 }
 
-var areaPerimetro = () => {
-  var radio = document.getElementById('radio')
-  var areaPerimetroContainer = document.getElementById('areaPerimetroContainer')
-  var resultadoArea, resultadoPerimetro, radioP
-  radioP = parseInt(radio.value)
-  resultadoPerimetro = (2 * (Math.PI) * radioP);
-  resultadoArea = Math.PI * (Math.pow(radioP, 2));
-  areaPerimetroContainer.innerHTML = "El Area es igual a: " + resultadoArea.toFixed(2) + " y el perimetro es igual a: "+ resultadoPerimetro.toFixed(2)
+function area(){
+  let radio = document.getElementById('radio')
+  let r = parseInt(radio.value)
+  let areayPerimetro = document.getElementById('areayPerimetro')
+  let area = (Math.PI * (r**2)).toFixed(2)
+  let perimetro = (r * (Math.PI * 2)).toFixed(2)
+  areayPerimetro.innerHTML = `El área es: ${area} y el perimetro es ${perimetro}`
 }
 
-var obtenerPromedio = () => {
-  var nota1 = document.getElementById('nota1')
-  var nota1P = parseInt(nota1.value)
-  var nota2 = document.getElementById('nota2')
-  var nota2P = parseInt(nota2.value)
-  var nota3 = document.getElementById('nota3')
-  var nota3P = parseInt(nota3.value)
-  var nota4 = document.getElementById('nota4')
-  var nota4P = parseInt(nota4.value)
-  var unidades = 4
-  var promedioContainer = document.getElementById('promedioContainer')
-  var resultadoPromedio = ((nota1P + nota2P + nota3P + nota4P) / unidades)
-  promedioContainer.innerHTML = "El promedio de las notas ingresadas es: " + resultadoPromedio
+function promedio(){
+  let n1 = document.getElementById('n1')
+  n1 = parseInt(n1.value)
+  let n2 = document.getElementById('n2')
+  n2 = parseInt(n2.value)
+  let n3 = document.getElementById('n3')
+  n3 = parseInt(n3.value)
+  let n4 = document.getElementById('n4')
+  n4 = parseInt(n4.value)
+  let promedioF = (n1 + n2 + n3 + n4) / 4
+  let mostrarPromedio = document.getElementById('mostrarPromedio')
+  mostrarPromedio.innerHTML = `El promedio de las cuatro notas es: ${promedioF}`
 }
 
-var obtenerPulgafas = () => {
-  var centimetros = document.getElementById('ingresoCentimetros')
-  var cmtsToIn = parseInt(centimetros.value)
-  var pulgadas = (cmtsToIn / 2.54)
-  pulgadasContainer.innerHTML = "El valor ingresado es igual a " + pulgadas.toFixed(2) + " pulgadas"
+function longitud() {
+  let cm = document.getElementById('cm')
+  cm = parseInt(cm.value)
+  let longitud = document.getElementById('longitud')
+  let cmToInch = (cm / 2.54).toFixed(2)
+  longitud.innerHTML = `La longitud es ${cmToInch} Pulgadas`
 }
 
-var obtenerInverso = () => {
-  var numberValue = document.getElementById('inversos')
-  var numberOne = numberValue.value[0]
-  var numberTwo = numberValue.value[1]
-  var numberThree = numberValue.value[2]
-  var containerInversos = document.getElementById('containerInversos')
-  containerInversos.innerHTML = "El inverso del valor ingresado es " +numberThree+numberTwo+numberOne
-}
-
-var obtenerHipotenusa = () => {
-  var catetoA = document.getElementById('catetoA')
-  var CA = parseInt(catetoA.value)
-  var catetoB = document.getElementById('catetoB')
-  var CB = parseInt(catetoB.value)
-  var hipotenusaF = Math.sqrt(Math.pow(CA,2) + Math.pow(CB,2))
-  var containerHipotenusa = document.getElementById('containerHipotenusa')
-  containerHipotenusa.innerHTML = "El valor de la Hipotenusa es: " + hipotenusaF.toFixed(2)
+function mostrarInverso() {
+  let original = document.getElementById('original').value
+  let mostrarInverso = document.getElementById('mostrarInverso')
+  mostrarInverso.innerHTML = `El inverso del número es: ${original.charAt(2)}${original.charAt(1)}${original.charAt(0)}`
 }
